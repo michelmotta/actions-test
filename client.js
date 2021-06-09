d9cd5706806f1fd566737d820afed58825eb3951
Michel Motta da Silva <michelmottadasilva@gmail.com>
2021-06-09 09:08:14 -0400

    Add host name direcly

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 const pgclient = new Client({
-    host: process.env.POSTGRES_HOST,
+    host: 'localhost',
     port: process.env.POSTGRES_PORT,
     user: 'postgres',
     password: 'postgres',
