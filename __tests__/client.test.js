1701f9d92b2d833d731481ec43b97a71512b394a
Michel Motta da Silva <michelmottadasilva@gmail.com>
2021-06-09 15:05:21 -0400

    Alterações de teste

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -8,7 +8,7 @@ describe("Database Conection Test", () => {
         
         const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, email VARCHAR(40))'
         const text = 'INSERT INTO student(firstname, email) VALUES($1, $2) RETURNING *'
-        const values = ['Teste', 'testet@github.com']
+        const values = ['Teste', 'teste@github.com']
         
         pgclient.query(table, (err, res) => {
             if (err) throw err
