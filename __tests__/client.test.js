767e9fe526ba2a623bbc4f781b3ad95518e86c23
Michel Motta da Silva <michelmottadasilva@gmail.com>
2021-06-09 14:31:39 -0400

    Alterações de teste

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -1,16 +1,16 @@
 const { Client } = require('pg');
 
+const pgclient = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres'
+});
 
 describe("Database Conection Test", () => {
     
     beforeAll(async () => {
-        const pgclient = new Client({
-            host: process.env.POSTGRES_HOST,
-            port: process.env.POSTGRES_PORT,
-            user: 'postgres',
-            password: 'postgres',
-            database: 'postgres'
-        });
         
         pgclient.connect();
         
